baf26051ab6bc177c1ada94ab524a55e2a50c67a
Oogway <nmola2003@gmail.com>
2024-04-24 10:45:23 +0200

    feat: add server express

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+
+const app = express();
+
+app.use((req, res, next) => {
+  console.log("Time: ", Date.now());
+  next();
+});
+
+app.use("/request-type", (req, res, next) => {
+  console.log("Request type: ", req.method);
+  next();
+});
+
+app.use("/public", express.static("public"));
+
+app.get("/", (req, res) => {
+  res.send("Successful response.");
+});
+
+app.listen(3000, () => console.log("Example app is listening on port 3000."));
