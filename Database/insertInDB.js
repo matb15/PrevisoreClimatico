a1cc8dc05b1fcc0f248d782f99c5a24ef2ee2517
Nikolaj-glitch <75386723+Nikolaj-glitch@users.noreply.github.com>
2024-04-30 09:34:06 +0200

    Database

---
+++ Database/insertInDB.js
@@ -0,0 +1,21 @@
+import { Console } from 'console'
+import { MongoClient } from 'mongodb'
+
+async function InsertInDocument(url, nomeDB, collectionName, document) {
+
+    const client = new MongoClient(url)
+
+    try {
+        await client.connect()
+        const db = client.db(nomeDB)
+        const collectionSearched = db.collection(collectionName)
+        const InsertInDocument = await collectionSearched.insertMany(document)
+        console.log(document);
+        return InsertInDocument.insertedId
+    }
+    finally {
+        await client.close()
+    }
+}
+export default InsertInDocument
+
