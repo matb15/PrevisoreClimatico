716def63eea943106edded95add7696f29920343
Nikolaj-glitch <75386723+Nikolaj-glitch@users.noreply.github.com>
2024-05-08 08:40:53 +0200

    Implementato l'aggiunta elementi al DB con mongoose

--- backend/Database/insertInDB.js
+++ backend/Database/insertInDB.js
@@ -1,5 +1,13 @@
 import mongoose from "mongoose";
-import dataWeather from "./schema/weather";
-async function addInDB(cityName,countryCode, object) {
-    
+import connessione from "./connessioneDB.js";
+import dataWeather from "./schema/weather.js";
+
+async function insertIntoDB(cityName, date) {
+    connessione()
+    const collezione = mongoose.model('dataWeather', dataWeather, 'Prova')
+    let tempo = await collezione.create({ "cityName": cityName, "date": date })
+    console.log(tempo)
+    mongoose.disconnect()
 }
+
+insertIntoDB("San PietroBurgo", "25/06/2003")
