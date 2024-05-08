77ba647d028aadbd16bf09045d9e45c76d47a0c3
Oogway <nmola2003@gmail.com>
2024-05-08 20:20:56 +0200

    fix: data minicard

--- frontend/src/components/MiniWeatherCard.js
+++ frontend/src/components/MiniWeatherCard.js
@@ -2,11 +2,37 @@ import React from "react";
 import "../css/WeatherCard.css";
 
 function MiniWeatherCard({ data }) {
+  let date = [];
+
+  function getMonthName(month) {
+    const monthNames = [
+      "gennaio",
+      "febbraio",
+      "marzo",
+      "aprile",
+      "maggio",
+      "giugno",
+      "luglio",
+      "agosto",
+      "settembre",
+      "ottobre",
+      "novembre",
+      "dicembre",
+    ];
+    return monthNames[month];
+  }
+
+  for (let i = 0; i < data.time.length; i++) {
+    let app = new Date(data.time[i]);
+    app = app.getDate() + " " + getMonthName(app.getMonth());
+    date.push(app);
+  }
+
   return (
     <div className="mini-container">
       <div className="mini-weather-card">
         <div className="mini-weather-info">
-          <h3>{data.time[0]}</h3>
+          <h3>{date[0]}</h3>
           <div className="mini-weather-details">
             <p>
               min: {data.temperature_2m_min[0]}°C | max:{" "}
@@ -17,7 +43,7 @@ function MiniWeatherCard({ data }) {
       </div>
       <div className="mini-weather-card">
         <div className="mini-weather-info">
-          <h3>{data.time[1]}</h3>
+          <h3>{date[1]}</h3>
           <div className="mini-weather-details">
             <p>
               min: {data.temperature_2m_min[1]}°C | max:{" "}
@@ -28,7 +54,7 @@ function MiniWeatherCard({ data }) {
       </div>
       <div className="mini-weather-card">
         <div className="mini-weather-info">
-          <h3>{data.time[2]}</h3>
+          <h3>{date[2]}</h3>
           <div className="mini-weather-details">
             <p>
               min: {data.temperature_2m_min[2]}°C | max:{" "}
@@ -39,7 +65,7 @@ function MiniWeatherCard({ data }) {
       </div>
       <div className="mini-weather-card">
         <div className="mini-weather-info">
-          <h3>{data.time[3]}</h3>
+          <h3>{date[3]}</h3>
           <div className="mini-weather-details">
             <p>
               min: {data.temperature_2m_min[3]}°C | max:{" "}
@@ -50,7 +76,7 @@ function MiniWeatherCard({ data }) {
       </div>
       <div className="mini-weather-card">
         <div className="mini-weather-info">
-          <h3>{data.time[4]}</h3>
+          <h3>{date[4]}</h3>
           <div className="mini-weather-details">
             <p>
               min: {data.temperature_2m_min[4]}°C | max:{" "}
@@ -61,7 +87,7 @@ function MiniWeatherCard({ data }) {
       </div>
       <div className="mini-weather-card">
         <div className="mini-weather-info">
-          <h3>{data.time[5]}</h3>
+          <h3>{date[5]}</h3>
           <div className="mini-weather-details">
             <p>
               min: {data.temperature_2m_min[5]}°C | max:{" "}
@@ -72,7 +98,7 @@ function MiniWeatherCard({ data }) {
       </div>
       <div className="mini-weather-card">
         <div className="mini-weather-info">
-          <h3>{data.time[6]}</h3>
+          <h3>{date[6]}</h3>
           <div className="mini-weather-details">
             <p>
               min: {data.temperature_2m_min[6]}°C | max:{" "}
